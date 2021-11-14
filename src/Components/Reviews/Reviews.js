3dd94e6b7251115572250cf88a2477dd83063cf8
unknown <mdsumonaliceng@gmail.com>
2021-11-14 17:23:27 +0600

    1

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Reviews = () => {
+    const { user } = useFirebase();
+    const initialInfo = { Name: user.displayName, email: user.email, phone: '' }
+    const [review, setReview] = useState(initialInfo);
+  
+  
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newInfo = { ...review };
+        newInfo[field] = value;
+        setReview(newInfo);
+    }
+    const handleReviewSubmit = e => {
+        const userReview = { review};
+      
+        fetch('https://damp-shore-20321.herokuapp.com/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userReview )
+        })
+            .then(res => res.json())
+            .then(data => {
+                
+                    console.log(data);
+                   
+               
+            })
+
+        e.preventDefault()
+    }
     return (
         <div>
             <h2>please give your opinion:</h2>
-            <form onSubmit>
+            <form onSubmit={handleReviewSubmit}>
             <div class="mb-3">
     <textarea type="name" placeholder="give your review"
     name="name"
+    onBlur={handleOnBlur}
     class="form-control" />
   </div>
   <button type="submit" class="btn btn-primary">submit</button>
