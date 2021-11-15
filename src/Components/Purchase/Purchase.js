f8dbdb4ba7fa75d047fb96ade609e8f8c765b8ea
unknown <mdsumonaliceng@gmail.com>
2021-11-15 09:22:41 +0600

    1

--- src/Components/Purchase/Purchase.js
+++ src/Components/Purchase/Purchase.js
@@ -1,6 +1,7 @@
+import React,{ useState,useEffect} from 'react';
 import axios from 'axios';
 import { useForm } from "react-hook-form";
-import './Purchase';
+import './purchase.css';
 import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
@@ -9,12 +10,12 @@ import useAuth from '../../hooks/useAuth';
 const Purchase = () => {
     const {bikeId } = useParams();
     const { user } = useAuth();
-//     const[bike ,setBike] =useState({})
-// useEffect(() => {
-//     fetch(`https://damp-shore-20321.herokuapp.com/bikes/${bikeId}`)
-// .then(res => res.json())
-// .then(data => setBike(data))
-// },[])
+    const[bike ,setBike] =useState({})
+useEffect(() => {
+    fetch(`https://damp-shore-20321.herokuapp.com/bikes/${bikeId}`)
+.then(res => res.json())
+.then(data => setBike(data))
+},[])
     
    
 
@@ -34,8 +35,14 @@ const Purchase = () => {
     }
     return (
         <div>
+           <div className="bike">
            <h2> product id: {bikeId}</h2>
-
+<h5>price:{bike.price}</h5>
+<img src={bike.img} alt="" />
+           
+          
+           <p className="px-5">description:{bike.description}</p>
+           </div>
             <h3>by email:{user?.email}</h3>
             <h3>by name:{user?.displayName}</h3>
             <h2>customer details</h2>
