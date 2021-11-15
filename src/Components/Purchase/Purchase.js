704c5e1eccfe840f38d76a786999d59443075733
unknown <mdsumonaliceng@gmail.com>
2021-11-15 08:31:13 +0600

    1

--- src/Components/Purchase/Purchase.js
+++ src/Components/Purchase/Purchase.js
@@ -1,12 +1,14 @@
-import React , { useState ,useEffect}from 'react';
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import './Purchase';
 import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
-import useFirebase from '../../hooks/useFirebase';
 
 const Purchase = () => {
     const {bikeId } = useParams();
+    const { user } = useAuth();
 //     const[bike ,setBike] =useState({})
 // useEffect(() => {
 //     fetch(`https://damp-shore-20321.herokuapp.com/bikes/${bikeId}`)
@@ -14,13 +16,38 @@ const Purchase = () => {
 // .then(data => setBike(data))
 // },[])
     
-    const { user } = useAuth();
+   
+
+
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+
+        axios.post('https://damp-shore-20321.herokuapp.com/customers', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert('details added successfully');
+                    reset();
+                }
+            })
+    }
     return (
         <div>
-           <h2> booking id: {bikeId}</h2>
+           <h2> product id: {bikeId}</h2>
 
             <h3>by email:{user?.email}</h3>
             <h3>by name:{user?.displayName}</h3>
+            <h2>customer details</h2>
+           <div className="add-one">
+           <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder="Full Name" />
+                <textarea {...register("address")} placeholder="address" />
+                <input type="number" {...register("phone")} placeholder="phone" />
+               
+                <input type="submit" />
+            </form>
+           </div>
             <Link to="/myorder"><Button>PlaceOrder</Button></Link>
         </div>
     );
